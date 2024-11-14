90ffd2eccb0dfe20cd71ff234a1534547abe862a
ConChimNon0102 <nguyenvo122005@gmail.com>
2024-11-14 17:01:21 +0700

    Homepage full
    full homepage

--- Homepage/script.js
+++ Homepage/script.js
@@ -90,3 +90,18 @@ new Swiper('.banner-wrapper', {
         },
     }
 });
+
+
+// tab container
+const tabs = document.querySelectorAll('.tab-btn');
+const all_content = document.querySelectorAll('.content');
+
+tabs.forEach((tab, index)=>{
+    tab.addEventListener('click', ()=>{
+        tabs.forEach(tab=>{tab.classList.remove('active')});
+        tab.classList.add('active');
+
+        all_content.forEach(content=>{content.classList.remove('active')});
+        all_content[index].classList.add('active');
+    })
+})
