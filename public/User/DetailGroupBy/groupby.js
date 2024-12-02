4ace42612e39cc848a175f5f2446906583f4f19f
22120209 <minhpqdp02@gmail.com>
2024-12-02 18:49:41 +0700

    xong detail groupby

--- public/User/DetailGroupBy/groupby.js
+++ public/User/DetailGroupBy/groupby.js
@@ -1,4 +1,5 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("Initial window.data:", window.data);
   new Vue({
     el: "#app",
     data: {
@@ -7,13 +8,20 @@ document.addEventListener("DOMContentLoaded", function() {
       check: false,
       quanlity: 1,
       images: [
-        "/image/detail_groupby/1.jpg","/image/detail_groupby/2.jpg",
-        "/image/detail_groupby/3.jpg","/image/detail_groupby/4.jpg",
-        "/image/detail_groupby/5.jpg","/image/detail_groupby/6.jpg",
-        "/image/detail_groupby/7.jpg","/image/detail_groupby/8.jpg",
-        "/image/detail_groupby/9.jpg","/image/detail_groupby/10.jpg",
-        "/image/detail_groupby/11.jpg","/image/detail_groupby/12.jpg"
+        "/image/detail_groupby/1.jpg",
+        "/image/detail_groupby/2.jpg",
+        "/image/detail_groupby/3.jpg",
+        "/image/detail_groupby/4.jpg",
+        "/image/detail_groupby/5.jpg",
+        "/image/detail_groupby/6.jpg",
+        "/image/detail_groupby/7.jpg",
+        "/image/detail_groupby/8.jpg",
+        "/image/detail_groupby/9.jpg",
+        "/image/detail_groupby/10.jpg",
+        "/image/detail_groupby/11.jpg",
+        "/image/detail_groupby/12.jpg",
       ],
+      getDB: window.data.dataGroupByProduct[0],
     },
     methods: {
       changeMainImage(image) {
@@ -32,8 +40,14 @@ document.addEventListener("DOMContentLoaded", function() {
         this.check = !this.check;
       },
     },
+    computed: {
+      formattedDescription() {
+        return this.getDB.specification.replace(/\n/g, '<br>');
+      },
+    },
     mounted() {
       console.log("Vue instance has been mounted and script is loaded.");
-    }
+      console.log("Data from server (window.data):", this.getDB);
+    },
   });
 });
