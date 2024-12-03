29ae583d3a7ae087e6ee2e756495b3c3466f27b1
22120209 <minhpqdp02@gmail.com>
2024-12-03 18:55:31 +0700

    chinh sua nho get api image cua 1 san pham

--- routes/User/detailProduct.route.js
+++ routes/User/detailProduct.route.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const detailProductController = require('../../app/controllers/User/detailProductController');
+const apiImageController = require("../../app/controllers/User/getApiImageController");
+
 
 router.get('/', detailProductController.showDetailProduct);
+router.get('/image/api', apiImageController.createApi.bind(apiImageController));
 
 module.exports = router;
