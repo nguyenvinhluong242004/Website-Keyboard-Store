005683e5ad90283cf885acf2c826a90f59376a78
22120209 <minhpqdp02@gmail.com>
2024-12-15 21:31:28 +0700

    them duong dan cho submit

--- routes/Admin/orderItem.route.js
+++ routes/Admin/orderItem.route.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const orderController = require('../../app/controllers/Admin/orderController');
 
 router.get ('/',orderController.order);
+router.post ('/insert',orderController.createGroupBy);
+router.post ('/insert-groupbyproduct',orderController.createGroupByProduct);
+router.get ('/type',orderController.getType);
 
 module.exports= router;
