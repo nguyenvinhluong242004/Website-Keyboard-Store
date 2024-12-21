7d560594de92030281279f725d28f507ce236a34
22120209 <minhpqdp02@gmail.com>
2024-12-21 20:05:14 +0700

    chinh sua route them id product

--- routes/User/detailGroupby.route.js
+++ routes/User/detailGroupby.route.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const detailGroupbyController = require('../../app/controllers/User/detailGroupbyController');
 
-router.get('/', detailGroupbyController.showDetailGroupby);
+router.get('/:id', detailGroupbyController.showDetailGroupby);
 
 module.exports = router;
