08720623f163893b8de680050638ebace72349e8
22120209 <minhpqdp02@gmail.com>
2024-12-21 20:07:18 +0700

    sua loai controller cho phu hop

--- app/controllers/User/detailGroupbyController.js
+++ app/controllers/User/detailGroupbyController.js
@@ -3,22 +3,28 @@ const controller = {};
 
 // Render Kit Phim
 controller.showDetailGroupby = async (req, res) => {
+    const id = req.params.id || 1;
+    const visibleCount = req.query.visibleCount || 1; // Lấy visibleCount từ query
+
     try {
-        // Lấy dữ liệu từ model
-        const data = await getGroupByProduct(req, res);
+        // Lấy dữ liệu từ model và truyền visibleCount
+        const data = await getGroupByProduct(id, visibleCount);
+
+        console.log("data controller:", data);
 
         // Render view với dữ liệu
         res.render('User/detail-groupby', {
             layout: 'layoutUser',
             title: 'DetailGroupby',
             customHead: `
-                <link rel="stylesheet" href="User/DetailGroupBy/detail-groupby.css">
-                <script defer src="User/DetailGroupBy/groupby.js"></script>
+                <link rel="stylesheet" href="/User/DetailGroupBy/detail-groupby.css">
+                <script defer src="/User/DetailGroupBy/groupby.js"></script>
             `,
+            idGroupby:id,
             data: JSON.stringify(data) // Chuyển dữ liệu sang JSON để Vue.js sử dụng
         });
     } catch (error) {
-        console.error('Error fetching data:', error.message);
+        console.error('Error fetching data from model:', error.message);
         res.status(500).send('Error fetching data: ' + error.message);
     }
 };
