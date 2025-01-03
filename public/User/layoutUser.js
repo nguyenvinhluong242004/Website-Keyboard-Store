c4d687756c4fdf820fee461ce59a6020689a7939
22120209 <minhpqdp02@gmail.com>
2025-01-04 04:31:04 +0700

    fix header

--- public/User/layoutUser.js
+++ public/User/layoutUser.js
@@ -1,59 +1,85 @@
-
 new Vue({
-    el: '#footer',
-    delimiters: ['[[', ']]'],
-    data: {
-        darkMode: false,
-        newsletterEmail: ''
+  el: "#footer",
+  delimiters: ["[[", "]]"],
+  data: {
+    darkMode: false,
+    newsletterEmail: "",
+  },
+  methods: {
+    toggleDarkMode() {
+      this.darkMode = !this.darkMode;
+      $("html").attr("data-bs-theme", this.darkMode ? "dark" : "light");
     },
-    methods: {
-        toggleDarkMode() {
-            this.darkMode = !this.darkMode;
-            $('html').attr('data-bs-theme', this.darkMode ? 'dark' : 'light');
-        },
 
-        subscribeNewsletter() {
-            if (!this.validateEmail()) {
-                return;
-            }
-            axios.post('/subscribe-news-letter/api', { email: this.newsletterEmail })
-                    .then(response => {
-                        alert(response.data.message);
-                    })
-                    .catch(error => {
-                        alert('Subscription failed. Please try again.');
-                    });
-        },
+    subscribeNewsletter() {
+      if (!this.validateEmail()) {
+        return;
+      }
+      axios
+        .post("/subscribe-news-letter/api", { email: this.newsletterEmail })
+        .then((response) => {
+          alert(response.data.message);
+        })
+        .catch((error) => {
+          alert("Subscription failed. Please try again.");
+        });
+    },
 
-        validateEmail() {
-            const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-            if (!emailPattern.test(this.newsletterEmail)) {
-                alert('Please enter a valid email address.');
-                return false;
-            } else if (!this.newsletterEmail.endsWith('.com')) {
-                alert('Email must end with .com')
-                return false;
-            } else {
-                return true;
-            }
-        }
-
-    }
+    validateEmail() {
+      const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!emailPattern.test(this.newsletterEmail)) {
+        alert("Please enter a valid email address.");
+        return false;
+      } else if (!this.newsletterEmail.endsWith(".com")) {
+        alert("Email must end with .com");
+        return false;
+      } else {
+        return true;
+      }
+    },
+  },
 });
 
 new Vue({
-    el: '#header',
-    data: {
-        searchQuery: ''
+  el: "#header",
+  delimiters: ["[[", "]]"],
+  data: {
+    searchQuery: "",
+    categorys:[],
+  },
+  mounted(){
+    this.fetchCategory();
+  },
+  methods: {
+    search() {
+      if (this.searchQuery === "") {
+        alert("Please enter a search query.");
+        return;
+      }
+      const encodedQuery = encodeURIComponent(this.searchQuery);
+      window.location.href = `/search?search=${encodedQuery}`;
     },
-    methods: {
-        search() {
-            if (this.searchQuery === '') {
-                alert('Please enter a search query.');
-                return;
+    fetchCategory() {
+      const url = "/api/category";
+      fetch(url)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          return response.json();
+        })
+        .then((data) => {
+            if(data.success){
+                this.categorys = data.data;
+                console.log(this.categorys);
+            }else{
+                console.log('Dont get data category success');
             }
-            const encodedQuery = encodeURIComponent(this.searchQuery);
-            window.location.href = `/search?search=${encodedQuery}`;
-        },
-    }
+         
+        })
+        .catch((error) => {
+          console.error("There was a problem with the fetch operation:", error);
+        });
+    },
+  },
 });
