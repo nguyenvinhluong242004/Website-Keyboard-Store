20c8d9e4ec8a58749faabe80830ea327cb3e80d5
22120209 <minhpqdp02@gmail.com>
2025-01-04 08:31:04 +0700

    fix review for detail product

--- routes/User/pathImage.route.js
+++ routes/User/pathImage.route.js
@@ -5,5 +5,7 @@ const controller = require('../../app/controllers/User/findPathImageController')
 router.get('/path_img', controller.getPathImg);
 router.get('/indexLastProduct', controller.indexLastProduct);
 router.get('/category', controller.allcategory);
+router.get('/review/:id', controller.review);
+router.post('/give-review/', controller.giveReview);
 
 module.exports = router;
