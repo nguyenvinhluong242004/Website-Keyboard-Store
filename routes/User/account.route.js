5602c78ce040fb08afc394fec553c81aaff093c5
22120197 <luongnguyen02042004@gmail.com>
2025-01-08 21:58:48 +0700

    update logic user cancel order before shipped

--- routes/User/account.route.js
+++ routes/User/account.route.js
@@ -2,6 +2,7 @@ const express = require('express'); // Web framework cho Node.js
 const  router = express.Router();
 const accountController = require('../../app/controllers/User/AccountController');
 
+router.post('/cancel-order/api', accountController.callAPIAccountCancelOrder);
 router.post('/get-info/api', accountController.callAPIAccountGetInfo);
 router.post('/delete-address/api', accountController.callAPIAccountDeleteAddress);
 router.post('/add-address/api', accountController.callAPIAccountAddAddress);
